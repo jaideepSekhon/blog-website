a0dc3698c7d0d3f2aa5a0f5c8db7e9a99622eea5
Jaideep Singh Sekhon <144608445+jaideepSekhon@users.noreply.github.com>
2026-04-23 15:47:20 +0530

    Delete backend directory

--- backend/src/main/java/com/example/blog_backend/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.blog_backend.repository;
-
-import com.example.blog_backend.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
